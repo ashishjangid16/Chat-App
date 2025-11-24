5819bd41dd0a6f879bd35ddfed157916d1e92e79
Ashish Kumar Jangid <your-email@example.com>
2025-11-24 20:48:47 +0530

    Add root .gitignore

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import { AuthContextProvider } from './context/Authcontext.jsx'
-import { SocketContextProvider } from './context/SocketContext.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <AuthContextProvider>
-      <SocketContextProvider>
-         <App />
-      </SocketContextProvider>
-    </AuthContextProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
