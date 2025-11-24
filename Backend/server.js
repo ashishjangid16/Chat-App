f5e797bbec58592b285fc9a38096472f60dfec17
Ashish Kumar Jangid <your-email@example.com>
2025-11-24 20:47:25 +0530

    Add root .gitignore

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 import path from 'path';
 import express from "express";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
